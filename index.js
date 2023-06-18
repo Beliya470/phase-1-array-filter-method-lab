ca98433320ae3ef299e224f81587d668374543e0
Beliya470 <anneanziay78@gmail.com>
2023-06-18 16:47:44 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-// Code your solution here
+function findMatching(drivers, query) {
+  return drivers.filter((driver) =>
+    driver.toLowerCase() === query.toLowerCase()
+  );
+}
+
+function fuzzyMatch(drivers, letters) {
+  return drivers.filter((driver) =>
+    driver.toLowerCase().startsWith(letters.toLowerCase())
+  );
+}
+
+function matchName(drivers, name) {
+  return drivers.filter((driver) => driver.name === name);
+}
+
+// Test data
+const drivers = [
+  "John",
+  "Mike",
+  "Sarah",
+  "Alice",
+  "Matt",
+  "Emily",
+  "Daniel",
+  "Emma",
+];
+
+console.log("findMatching:", findMatching(drivers, "John")); // should return ["John"]
+console.log("fuzzyMatch:", fuzzyMatch(drivers, "Mi")); // should return ["Mike", "Mike"]
+console.log(
+  "matchName:",
+  matchName(
+    [
+      { name: "John", hometown: "New York" },
+      { name: "Mike", hometown: "Chicago" },
+      { name: "Sarah", hometown: "Los Angeles" },
+    ],
+    "John"
+  )
+); // should return [{ name: "John", hometown: "New York" }]
